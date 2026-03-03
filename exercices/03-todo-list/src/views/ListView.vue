6c5c28c139b7506dd857689c83c69fa6b0b360dd
macdrien <adrien.bouyssou@gmail.com>
2026-03-03 23:36:02 +0100

    Move todos in an asset file

--- exercices/03-todo-list/src/views/ListView.vue
+++ exercices/03-todo-list/src/views/ListView.vue
@@ -1,13 +1,6 @@
 <script setup lang="js">
-import { ref } from 'vue'
+import todos from '@/assets/todos.js'
 import TodoList from '@/components/todo/TodoList.vue'
-
-const todos = ref([
-  { id: 1, title: 'Apprendre le VueJS', status: 'In progress' },
-  { id: 2, title: 'Apprendre le ReactJS', status: 'Todo' },
-  { id: 3, title: "Apprendre l'Angular", status: "Won't do" },
-  { id: 4, title: 'Devenir le maître du monde', status: 'Done' },
-])
 </script>
 
 <template>
